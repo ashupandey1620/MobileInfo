679aa6791213ddfa1547d22ed89667edd3b8d515
Ashutosh Pandey <ashupandey1620@gmail.com>
2023-04-22 23:22:08 +0530

    java file

--- app/src/main/java/com/example/mobileinfo/MainActivity.java
+++ app/src/main/java/com/example/mobileinfo/MainActivity.java
@@ -8,6 +8,10 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.Context;
 import android.graphics.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.telephony.TelephonyManager;
 
 import android.content.pm.PackageManager;
@@ -35,13 +39,11 @@ import androidx.core.app.ActivityCompat;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.List;
 
 import javax.microedition.khronos.egl.EGLConfig;
 
 public class MainActivity extends AppCompatActivity {
-
-
-
 
 
     private String manufacturer, modelName, modelNumber, mProcessorInfostr;
@@ -62,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView mIMEIInfo;
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,12 +75,12 @@ public class MainActivity extends AppCompatActivity {
         mStorage = findViewById(R.id.tv_storage);
         mBatteryLevel = findViewById(R.id.tv_battery_level);
 
-         mVersion = findViewById(R.id.tv_android_version);
-         mCameraMP=findViewById(R.id.tv_camera_mp);
-         mCameraAperture=findViewById(R.id.tv_camera_aperture);
-         mProcessorInfo=findViewById(R.id.tv_processor_info);
-         mGPUInfo=findViewById(R.id.tv_gpu_info);
-         mIMEIInfo=findViewById(R.id.tv_imei);
+        mVersion = findViewById(R.id.tv_android_version);
+        mCameraMP = findViewById(R.id.tv_camera_mp);
+        mCameraAperture = findViewById(R.id.tv_camera_aperture);
+        mProcessorInfo = findViewById(R.id.tv_processor_info);
+        mGPUInfo = findViewById(R.id.tv_gpu_info);
+        mIMEIInfo = findViewById(R.id.tv_imei);
 
         // Retrieve device information
         getDeviceInfo();
@@ -126,9 +127,6 @@ public class MainActivity extends AppCompatActivity {
 // Get the camera megapixel and aperture
 
 
-
-
-
         //processor
         mProcessorInfostr = Build.HARDWARE + " " + Build.BRAND + " " + Build.MODEL;
         Log.i(TAG, mProcessorInfostr);
@@ -138,20 +136,66 @@ public class MainActivity extends AppCompatActivity {
         //Gpu
 
 
-
-
         //imei
 
 
-
-
-
-
-
         //Sensors Data
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+
+
+
+       /** for (Sensor sensor : sensors) {
+            if (sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+            } else if (sensor.getType() == Sensor.TYPE_GYROSCOPE) {
+                sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+            }  else if (sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+                sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+            } else if (sensor.getType() == Sensor.TYPE_PROXIMITY) {
+                sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+            } else if (sensor.getType() == Sensor.TYPE_LIGHT) {
+                sensorManager.registerListener(sensorEventListener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+            }
+        }*/
 
 
     }
 
+    private final SensorEventListener sensorEventListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            switch (sensorEvent.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    float[] valuesAcc = sensorEvent.values;
+                    Log.i(TAG, "Accelerometer: X = " + valuesAcc[0] + ", Y = " + valuesAcc[1] + ", Z = " + valuesAcc[2]);
+                    break;
+                case Sensor.TYPE_GYROSCOPE:
+                    float[] valuesGyro = sensorEvent.values;
+                    Log.i(TAG, "Gyroscope: X = " + valuesGyro[0] + ", Y = " + valuesGyro[1] + ", Z = " + valuesGyro[2]);
+                    break;
+                case Sensor.TYPE_ROTATION_VECTOR:
+                    float[] valuesRot = sensorEvent.values;
+                    Log.i(TAG, "Rotation Vector: X = " + valuesRot[0] + ", Y = " + valuesRot[1] + ", Z = " + valuesRot[2]);
+                    break;
+                case Sensor.TYPE_PROXIMITY:
+                    float valueProx = sensorEvent.values[0];
+                    Log.i(TAG, "Proximity: " + valueProx);
+                    break;
+                case Sensor.TYPE_LIGHT:
+                    float valueLight = sensorEvent.values[0];
+                    Log.i(TAG, "Ambient Light: " + valueLight);
+                    break;
 
+            }
+
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+    };
 }
